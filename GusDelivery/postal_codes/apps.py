17cbeda9904530d6b557c3e170c380b0148942be
Fernando Cruz <lfcruz@udes.edu.mx>
2021-08-26 17:58:37 -0500

    Avances

---
+++ GusDelivery/postal_codes/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class PostalCodesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'postal_codes'
+    verbose_name = 'Códigos postales'
